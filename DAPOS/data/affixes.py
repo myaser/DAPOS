584477321c520ee942215b4f3654fff4b6dba518
myaser <me.MahmoudYaser@gmail.com>
2013-09-26 14:29:45 +0200

    travis setup

--- DAPOS/data/affixes.py
+++ DAPOS/data/affixes.py
@@ -15,14 +15,14 @@ suffixes = datrie.Trie(ARABIC_CHARS)
 # _path = os.path.abspath(os.path.join(__file__, os.path.pardir, 'affixes'))
 # _xml_khalil_prefixes = ET.parse(os.path.join(_path, 'KHALIL_prefixes.xml')).getroot()
 # _xml_dapos_prefixes = ET.parse(os.path.join(_path, 'DAPOS_prefixes.xml')).getroot()
-# 
+#
 # _xml_khalil_suffixes = ET.parse(os.path.join(_path, 'KHALIL_suffixes.xml')).getroot()
 # _xml_dapos_suffixes = ET.parse(os.path.join(_path, 'DAPOS_suffixes.xml')).getroot()
-# 
+#
 # for prefix in chain(_xml_khalil_prefixes.iter('prefixe'),
 #                     _xml_dapos_prefixes.iter('prefixe')):
 #     prefixes[prefix.attrib[u'unvoweledform']] = prefix.attrib['classe']
-# 
+#
 # for suffix in chain(_xml_khalil_suffixes.iter('suffixe'),
 #                     _xml_dapos_suffixes.iter('suffixe')):
 #     suffixes[suffix.attrib[u'unvoweledform'][::-1]] = suffix.attrib['classe']
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 DAPOS
 =====
+[![Build Status](https://travis-ci.org/myaser/DAPOS_corpus.png)](https://travis-ci.org/myaser/DAPOS_corpus)
+[![Coverage Status](https://coveralls.io/repos/myaser/DAPOS_corpus/badge.png?branch=master)](https://coveralls.io/r/myaser/DAPOS_corpus)
 
 Dialectal Arabic Part Of Speech Tagger
