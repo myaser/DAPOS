df40b00ae510f555f3045a030d2444691a83df1e
moemen <moemenology@gmail.com>
2013-09-30 13:40:31 +0200

    use text as pipeline stage

--- DAPOS/data/normalization.py
+++ DAPOS/data/normalization.py
@@ -3,7 +3,7 @@ print "    loading normalization patterns and constants"
 import re
 from chars import ARABIC_VOWELS
 
-emoticons_regex = re.compile("(?:\s|^)([bcopvxBCDILOPSX:っ@Q;°_>,Þ$#03578&%ಠ~\-\\/\*\]\[<\)\(\{\}'\.\^=\|]{2,9})(?=\s|$)")
+emoticons_regex = re.compile(r"(?:\s|^)([bcopvxBCDILOPSX:っ@Q;°_>,Þ$#03578&%ಠ~\-\\/\*\]\[<\)\(\{\}'\.\^=\|]{2,9})(?=\s|$)")
 punctuation_regex = re.compile(ur"([^\u0621-\u063A^\u0641-\u064A^\w\s]+)")
 digit_regex = re.compile(r'\b(\d+)\b')
 float_regex = re.compile(r'\b(\d+[.]\d+)\b')
@@ -11,6 +11,7 @@ clock_regex = re.compile(r'\b(\d{1,2}[:]\d{1,2})\b')
 date_regex = re.compile(r'\b(\d{1,2}[-/]\d{1,2}[-/]\d{2,4})\b')
 vowels_regex = re.compile('[' + "".join(ARABIC_VOWELS) + ']')
 tagged_text = re.compile(ur'([^\s^^]+<\w+>)')
+split_tag_regex = re.compile(ur'([^\s^^]+)<(\w+)>')
 
 EMOTICONS_TAG = 'EMO'
 PUNCTUATION_TAG = 'PUNC'
--- DAPOS/tag.py
+++ DAPOS/tag.py
@@ -1,42 +1,26 @@
 from pipeline import Pipeline
 from pipeline.utils import StopPipeline
-from functools import partial
+
 from DAPOS.processor.token.segmentation import Word
-
 from DAPOS.processor.norm.cleaner import remove_diacritics, split_emoticons, \
-    split_punctuation, split_digits, tagged_text
-
-import re
-split_tag_regex = re.compile(ur'([^\s^^]+)<(\w+)>')
+    split_punctuation, split_digits
+from DAPOS.data.normalization import split_tag_regex, tagged_text
 
 
-def generate_producer(producer, text):
-    '''
-        return producer for pipeline in the form it deal with
-    '''
-    return partial(producer, text)
-
-def generate_consumer(consumer):
-    '''it should get a function that get one arg, and this function resposble
-    for dealling with what to do with data
-    '''
-    return consumer
-
-def provide_text(text, stage):
+def provide_text(text):
     '''
     pipline producer
     '''
-    if stage < 1:
-        return (text, stage+1)
-    else:
+    if text is None:
         raise StopPipeline("limit reached")
+    return ((text, {}), None)
+
 
 def consume_text(text):
     '''consumer function
     it don't do any thing!
     '''
     return text
-
 
 def split_tag_or_iffex(element):
     '''
@@ -51,10 +35,10 @@ def split_tag_or_iffex(element):
 
 def tag(text):
     '''get the data in the form the user intered and return tagged words'''
-    pipeline = Pipeline(generate_producer(provide_text, text),
+    pipeline = Pipeline(provide_text,
         [remove_diacritics, split_emoticons, split_punctuation, split_digits],
-        generate_consumer(consume_text))
-    cleaned_text = pipeline.follow(0)[0]
+        consume_text)
+    cleaned_text = pipeline.follow(text).pop()
 
     words = [split_tag_or_iffex(word)
         for word in cleaned_text.split()
