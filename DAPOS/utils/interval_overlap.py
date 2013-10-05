d005cc4378ac0d0bc8deabd843b172bf808aede9
moemen <moemenology@gmail.com>
2013-10-05 13:28:42 +0200

    overlap over two groups of intervals utility

--- DAPOS/utils/interval_overlap.py
+++ DAPOS/utils/interval_overlap.py
@@ -1,3 +1,16 @@
+from banyan import SortedSet, OverlappingIntervalsUpdator
+
+
+def overlap(intervals1, intervals2):
+    intervals = SortedSet(intervals1,
+                          key_type = (int, int),
+                          updator=OverlappingIntervalsUpdator)
+
+    results = []
+    for other_interval in intervals2:
+        # TODO: PROBLEM: if intervals overlap in one point, it's counted here!
+        results += intervals.overlap(other_interval)
+    return results
 
 def overlap_with(interval, dic):
     for key in dic.keys():
